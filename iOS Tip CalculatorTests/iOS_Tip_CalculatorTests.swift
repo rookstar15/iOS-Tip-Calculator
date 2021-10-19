c31c2dc747df74c7695e8faf557d7a9f246f579e
Islam <taseenislam55@gmail.com>
2021-10-18 23:29:33 -0400

    ios
    tip

---
+++ iOS Tip CalculatorTests/iOS_Tip_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_Tip_CalculatorTests.swift
+//  iOS Tip CalculatorTests
+//
+//  Created by Imu Islam on 10/18/21.
+//
+
+import XCTest
+@testable import iOS_Tip_Calculator
+
+class iOS_Tip_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
